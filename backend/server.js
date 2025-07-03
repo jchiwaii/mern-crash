b139eaa9bc2439dcd1136367a6940171d36722d1
Chiwai <chiwai.kiriba@gmail.com>
2025-07-03 11:05:13 +0300

    initial setup + db

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connect } from "mongoose";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on http://localhost:3000");
+});
